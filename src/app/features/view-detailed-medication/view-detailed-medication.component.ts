61a5163b10228e74675d54c398fd02c787e5a743
Julio César Forero <jc.foreroo1@uniandes.edu.co>
2024-09-26 16:46:06 -0500

    Se desarrollan las pantallas View-Medication.component y View-detailed-medication

--- src/app/features/view-detailed-medication/view-detailed-medication.component.ts
+++ src/app/features/view-detailed-medication/view-detailed-medication.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';  // Importa MatIconModule
+import { MatButtonModule } from '@angular/material/button';  // Importa MatButtonModule
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-detailed-medication',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatButtonModule],  // Añadir MatIconModule aquí
   templateUrl: './view-detailed-medication.component.html',
-  styleUrl: './view-detailed-medication.component.scss'
+  styleUrls: ['./view-detailed-medication.component.scss'],
 })
 export class ViewDetailedMedicationComponent {
 
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/view-medication']);
+  }
+
+  deleteMedication() {
+    console.log('Eliminar medicamento');
+    // Aquí puedes agregar la lógica para eliminar el medicamento
+  }
 }
--- src/app/features/view-medication/view-medication.component.ts
+++ src/app/features/view-medication/view-medication.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router'
+import { MatCardModule } from '@angular/material/card';  // Importa MatCardModule
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-view-medication',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './view-medication.component.html',
   styleUrl: './view-medication.component.scss'
 })
-export class ViewMedicationComponent {
 
+export class ViewMedicationComponent {
+  medications = [
+    {
+      name: 'Nombre del medicamento',
+      subGroup: 'SubGrupo',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    },
+    {
+      name: 'Title',
+      subGroup: 'Subtitle',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    },
+    // Agrega más objetos de medicamentos aquí según sea necesario
+  ];
+
+  constructor(private router: Router) {}
+
+  viewMore(medication: any) {
+    console.log('Ver más detalles sobre:', medication);
+    // Aquí puedes agregar la lógica para redirigir a la vista detallada del medicamento
+    this.router.navigate(['/view-detailed-medication']);
+  }
 }
