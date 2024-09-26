175d4e7f1c4833e0e6f8048a7f22b8d5c21ee5b0
Julio César Forero <jc.foreroo1@uniandes.edu.co>
2024-09-26 16:58:33 -0500

    Se ajustan  las pantallas View-Medication.component y View-detailed-medication con nav-list

--- src/app/features/view-detailed-medication/view-detailed-medication.component.ts
+++ src/app/features/view-detailed-medication/view-detailed-medication.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';  // Importa MatIconModule
-import { MatButtonModule } from '@angular/material/button';  // Importa MatButtonModule
-import { Router } from '@angular/router';
+/*create-alarm.component.ts*/
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { NavListComponent } from '../../shared/nav-list/nav-list.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { AppComponent } from '../../app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-view-detailed-medication',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],  // Añadir MatIconModule aquí
+  imports: [
+    NavListComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDatepickerModule,
+    FormsModule,
+    AppComponent,
+    MatSlideToggleModule,
+    NgxMaterialTimepickerModule,
+    MatOptionModule,
+    MatSelectModule,
+    CommonModule
+    
+  ],
   templateUrl: './view-detailed-medication.component.html',
   styleUrls: ['./view-detailed-medication.component.scss'],
 })
--- src/app/features/view-medication/view-medication.component.ts
+++ src/app/features/view-medication/view-medication.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'
-import { MatCardModule } from '@angular/material/card';  // Importa MatCardModule
+/*create-alarm.component.ts*/
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { NavListComponent } from '../../shared/nav-list/nav-list.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { AppComponent } from '../../app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router'
+
 @Component({
   selector: 'app-view-medication',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    NavListComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDatepickerModule,
+    FormsModule,
+    AppComponent,
+    MatSlideToggleModule,
+    NgxMaterialTimepickerModule,
+    MatOptionModule,
+    MatSelectModule,
+    CommonModule
+    
+  ],
   templateUrl: './view-medication.component.html',
   styleUrl: './view-medication.component.scss'
 })
