c94196943843ae85c8ba748d21d7675732e07a3b
Julio César Forero <jc.foreroo1@uniandes.edu.co>
2024-09-26 16:22:39 -0500

    navegaciones a las pantallas respectivas

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -14,6 +14,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -37,25 +38,34 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    // Usando FormBuilder para crear el FormGroup
+  // Inyectar Router en el constructor
+  constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
   }
 
+  login() {
+    if  (true) //(this.loginForm.valid) 
+      {
+      // Aquí puedes agregar la lógica de autenticación
+      console.log('Autenticación exitosa');
+      this.router.navigate(['/view-medication']); // Redirigir después de la autenticación
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
+
+  forgotPassword() {
+    this.router.navigate(['/forgot-password']); // Redirigir a la página de recuperación de contraseña
+  }
+
   ngOnInit(): void {
     console.log('LoginComponent initialized');
   }
 
-  // Método que se ejecuta al enviar el formulario
   onLogin() {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-      // Aquí puedes implementar la lógica de autenticación
-    } else {
-      console.log('Formulario no válido');
-    }
+    this.login(); // Llamar al método login cuando se envíe el formulario
   }
 }
